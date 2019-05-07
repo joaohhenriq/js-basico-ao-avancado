34f86da0bf140b26dbf2b82c48e0d2fa3a4c1858
joaoh <joao_camargo10@hotmail.com>
2019-05-07 20:53:55 -0300

    Funções 02

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,22 @@
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(10,15)
+
+//////////////////////////////////////////
+
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(10,15))
+
+///////////////////////////////////////////
+
+const substracao = (a, b) => a - b
+
+console.log(substracao(15,10))
+
+const imprimir2 = a => console.log(a)
+imprimir2("Legal!!!")
