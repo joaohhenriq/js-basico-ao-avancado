0607f0c01573ff94fb935ce3889ea9f864abf97b
joaoh <joao_camargo10@hotmail.com>
2019-05-07 20:11:50 -0300

    Uso de variáveis booleanas

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,11 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+let nome = 'Lucas'
+console.log(nome || 'Desconhecido') // se o valor de nome for vazio, coloca desconhecido, caso contrario, mantem o nome
