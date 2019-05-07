109902f4daeaa12c0b2e0ff6035865c1e74851a6
joaoh <joao_camargo10@hotmail.com>
2019-05-06 22:01:36 -0300

    Tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'legal'
+console.log(qualquer)
+console.log(typeof qualquer) // pega o tipo da variável
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer) // pega o tipo da variável
+
+//tipagem fraca significa que uma mesma variável pode receber várias tipagens no mesmo bloco
