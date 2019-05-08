8cb73e333dc30f2a877be82e35716ff5de9a6f1b
joaoh <joao_camargo10@hotmail.com>
2019-05-08 20:17:56 -0300

    Objetos 2

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object) // Function
+console.log(typeof new Object) // Object
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
