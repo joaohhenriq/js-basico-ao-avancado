14efdff1a3f6044fee02192243117dd7fe2ac55d
joaoh <joao_camargo10@hotmail.com>
2019-05-12 14:28:31 -0300

    Herança 1

---
+++ objeto/heranca1.js
@@ -0,0 +1,14 @@
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200
+}
+
+console.log(ferrari.__proto__) // prototype é como se fosse a classe pai, em java
+console.log(ferrari.__proto__ === Object.prototype) // true
+console.log(volvo.__proto__ === Object.prototype) // true
+console.log(Object.prototype.__proto__) // null
