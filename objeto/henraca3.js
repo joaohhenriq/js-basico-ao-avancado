07f9cfcd5cce5b555496ab1a1cd9cda3b7023211
joaoh <joao_camargo10@hotmail.com>
2019-05-12 14:49:22 -0300

    herança 3

---
+++ objeto/henraca3.js
@@ -0,0 +1,22 @@
+const pai = { nome: 'Pedro', corCabelo: 'preto' }
+
+const filha1 = Object.create(pai)
+filha1.nome = 'Ana'
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai, {
+    nome: { value: 'Bia', writable: false, enumerable: true}
+})
+
+console.log(filha2.nome)
+filha2.nome = 'Carla'
+
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for(let key in filha2) {
+    filha2.hasOwnProperty(key) // se a propriedade pertence ou não ao objeto que estamos percorrendo no momento
+        ? console.log(key) // veio do objeto
+        : console.log(`Por herança: ${key}`) //veio da cadeia de protótipos
+
+}
